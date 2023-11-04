3a97ca5e60c277dfe41f7dcafac64bad75016268
WintrCat <wintrcat.wilson@gmail.com>
2023-11-04 20:38:28 +0000

    Prepare socket for deployment
    Fixed small script loading bug on client side and did some preparations for deployment

--- client/play/client/client.js
+++ client/play/client/client.js
@@ -1,7 +1,7 @@
 const roomCode = location.pathname.split("/").at(-1);
 const sessionToken = document.cookie.match(/(?<=survgame_session_token=).+?(?=;|$)/)[0];
 
-const socket = io("ws://localhost:8081");
+const socket = io(`${location.protocol.startsWith("https") ? "wss" : "ws"}://${location.hostname}:${parseInt(location.port) + 1}`);
 
 socket.on("disconnect", () => {
     
