59571f65d387a2a9cd0e40d720806f6a5fb7a76b
WintrCat <wintrcat.wilson@gmail.com>
2023-11-05 00:55:09 +0000

    Attempt to fix connection issue #4

--- client/play/client/client.js
+++ client/play/client/client.js
@@ -2,11 +2,6 @@ const roomCode = location.pathname.split("/").at(-1);
 const sessionToken = document.cookie.match(/(?<=survgame_session_token=).+?(?=;|$)/)[0];
 
 let socket = io("wss://socket.surv.wintrcat.uk/");
-setTimeout(() => {
-    if (!socket.connected) {
-        socket = io("ws://localhost:8081");
-    }
-}, 2000);
 
 socket.on("disconnect", () => {
     
