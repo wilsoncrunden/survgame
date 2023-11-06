00d2e090c870de548f4278a18268c09106f572c6
WintrCat <wintrcat.wilson@gmail.com>
2023-11-06 22:43:39 +0000

    Socket URL patch for last commit

--- client/play/client/client.js
+++ client/play/client/client.js
@@ -1,7 +1,7 @@
 const roomCode = location.pathname.split("/").at(-1);
 const sessionToken = document.cookie.match(/(?<=survgame_session_token=).+?(?=;|$)/)[0];
 
-let socket = io("ws://localhost:8081");
+let socket = io("wss://survsocket.wintrcat.uk/");
 
 socket.on("disconnect", () => {
     
